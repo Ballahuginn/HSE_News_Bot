9f6e99a7690f36de2db13fe0ad8fd16ce9596047
Pavel Barsukov <barsukovpt@outlook.com>
2016-10-25 16:39:10 +0300

    Самое крупное обновление с момента создания бота! Комбинация последних 5 новостей сразу из двух пабликов!

--- bot.py
+++ bot.py
@@ -4,18 +4,21 @@ import time
 from telebot import types
 
 
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot('237770898:AAEcM0wXgHK49izN4K3HhW99q5j0vfSoYJA')
 
 session = vk.Session()
 api = vk.API(session)
 
 vk_arr = []
+group_arr = []
+group_id_arr = []
 
 markup = types.ReplyKeyboardMarkup()
 markup1 = types.ReplyKeyboardMarkup()
 
 markup.row('HSE Official VK Group')
 markup.row('The Вышка')
+markup.row('Ok!')
 markup1.row('5 последних постов')
 markup1.row('Назад')
 
@@ -26,37 +29,67 @@ markup1.row('Назад')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, 'Привет! Выбери, откуда ты хочешь получить новости.', reply_markup=markup)
+    bot.send_message(message.chat.id, 'Привет! Выбери, откуда ты хочешь получить новости, а затем нажми "Ок"', reply_markup=markup)
 
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def news_source(message):
-    global group, group_id, vk_arr
+    global vk_arr, group_id_arr
+
     if message.text == 'HSE Official VK Group':
-        vk_arr = vkfunction('hse_university', '25205856')
+        if '25205856' not in group_id_arr:
+            # group_arr.append('hse_university')
+            group_id_arr.append('25205856')
+            bot.send_message(message.chat.id, 'Ты выбрал ' + message.text, reply_markup=markup)
+        else:
+            bot.send_message(message.chat.id, message.text + ' уже была выбрана')
+    if message.text == 'The Вышка':
+        if '66036248' not in group_id_arr:
+            # group_arr.append('thevyshka')
+            group_id_arr.append('66036248')
+            bot.send_message(message.chat.id, 'Ты выбрал ' + message.text, reply_markup=markup)
+        else:
+            bot.send_message(message.chat.id, message.text + ' уже была выбрана')
+    if message.text == 'Ok!':
         bot.send_message(message.chat.id, 'Что ты хочешь получить?', reply_markup=markup1)
-    elif message.text == 'The Вышка':
-        vk_arr = vkfunction('thevyshka', '66036248')
-        bot.send_message(message.chat.id, 'Что ты хочешь получить?', reply_markup=markup1)
+
     if message.text == '5 последних постов':
+        vk_arr = vkfunction(group_id_arr)
         if vk_arr:
-            for _l in vk_arr:
-                bot.send_message(message.chat.id, _l)
+            for _i in vk_arr:
+                bot.send_message(message.chat.id, _i)
         else:
             bot.send_message(message.chat.id, 'Вы не выбрали группу.')
     elif message.text == 'Назад':
-        bot.send_message(message.chat.id, 'Выбери, откуда ты хочешь получить новости.', reply_markup=markup)
+        bot.send_message(message.chat.id, 'Выбери, откуда ты хочешь получить новости, а затем нажми "Ок"', reply_markup=markup)
+        group_id_arr = []
 
 
-def vkfunction(vk_group, vk_group_id):
+def vkfunction(vk_id_arr=[]):
     arr_link = []
-    hse_official = api.wall.get(owner_id='-' + vk_group_id, count=5, filter='all')
-    for _i in hse_official:
-        if type(_i) != int:
-            if 'id' in _i:
-                    link = 'https://vk.com/' + vk_group + '?w=wall-' + vk_group_id + '_' + str(_i['id'])
+    vk_json_arr = []
+    link_count = 0
+    for _j in vk_id_arr:
+        group = api.wall.get(owner_id='-' + _j, count=6, filter='owner')
+        post_count = 0
+        for _k in group:
+            if type(_k) != int:
+                if 'id' in _k:
+                    if 'is_pinned' not in _k and post_count < 5:
+                        vk_json_arr.append(_k)
+                        post_count += 1
+
+    vk_sorted_arr = sorted(vk_json_arr, key=lambda k: int(k['date']), reverse=True)
+
+    for _n in vk_sorted_arr:
+        for _j in vk_id_arr:
+            if '-' + _j == str(_n['to_id']):
+                link = 'https://vk.com/wall-' + _j + '_' + str(_n['id'])
+                if link_count < 5:
                     arr_link.append(link)
-    return arr_link
+                    link_count += 1
+                    print(link)
+    return (arr_link)
 
 
 if __name__ == '__main__':
