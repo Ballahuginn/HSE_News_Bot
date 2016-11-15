fcc3d74a2fd8cfd37a7fc25f545134f80682890c
Pavel Barsukov <barsukovpt@outlook.com>
2016-11-15 18:56:02 +0300

    Работа через БД! (пока без подписки)

--- bot.py
+++ bot.py
@@ -42,25 +42,35 @@ for i in groups:
     markup.row(i[1])
 
 
-for i in groups:
-    posts = vk_api.wall.get(owner_id='-' + i[0], count=5, filter='owner')
-    for _k in posts['items']:
-        if type(_k) != int:
-            if 'id' in _k:
-                dbm.execute("SELECT * FROM Posts WHERE id = ?", (str(i[0])+'_'+str(_k['id']),))
-                post_in_table = dbm.fetchall()
-                if not post_in_table:
-                    dbm.execute("INSERT INTO Posts (id, gid, date) VALUES (?, ?, ?)",
-                                (str(i[0]) + '_' + str(_k['id']), str(i[0]), str(_k['date'])))
-                    databasem.commit()
+def get_post():
+    database = sqlite3.connect('HSE_BOT_DB.sqlite')
+    db = database.cursor()
+
+    db.execute("DELETE FROM Posts")
+
+    for i in groups:
+        posts = vk_api.wall.get(owner_id='-' + i[0], count=6, filter='owner')
+        for _k in posts['items']:
+            if type(_k) != int:
+                if 'id' in _k:
+                    db.execute("INSERT INTO Posts (id, gid, date) VALUES (?, ?, ?)",
+                               (str(i[0]) + '_' + str(_k['id']), str(i[0]), str(_k['date'])))
+                    # db.execute("SELECT * FROM Posts WHERE id = ?", (str(i[0])+'_'+str(_k['id']),))
+                    # post_in_table = db.fetchall()
+                    # if not post_in_table:
+                    #     db.execute("INSERT INTO Posts (id, gid, date) VALUES (?, ?, ?)",
+                    #                (str(i[0]) + '_' + str(_k['id']), str(i[0]), str(_k['date'])))
+                    #     database.commit()
+    database.commit()
+    database.close()
+    print('test')
+    t = threading.Timer(60, get_post)
+    t.start()
 
 
-# markup.row('HSE Official VK Group')
-# markup.row('The Вышка')
-# markup.row('THE WALL')
-# markup.row('HSE Press')
-# markup.row('Ингруп СтС НИУ ВШЭ')
-# markup.row('ТелеВышка')
+get_post()
+
+
 markup.row('Ok')
 markup.row('Главное меню')
 
@@ -72,7 +82,6 @@ markup1.row('Назад')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-
     database = sqlite3.connect('HSE_BOT_DB.sqlite')
     db = database.cursor()
 
@@ -95,33 +104,6 @@ def news_source(message):
 
     global vk_arr, group_id_arr
 
-    def print_vk_sub(msg, last_post_date):
-        last_posts = vk_sub(group_id_arr, last_post_date)
-        last_dates = []
-        ret_dates = []
-        for j in last_posts:
-            if j:
-                last_dates.append(j[0])
-                print(last_posts)
-                for i in range(1, len(j), 2):
-                    bot.send_message(msg.chat.id, j[i])
-            elif not j:
-                last_dates.append(None)
-
-        for k in range(0, len(last_post_date)):
-            if last_dates[k] is not None:
-                print(last_dates[k])
-                ret_dates.append(last_dates[k])
-            else:
-                print(last_post_date[k])
-                ret_dates.append(last_post_date[k])
-
-        print(ret_dates)
-        t = threading.Timer(3, print_vk_sub, [msg, ret_dates])
-        t.start()
-        if not sub_is_active:
-            t.cancel()
-
     if message.text == 'Выбрать группы':
         bot.send_message(message.chat.id, 'Выбери группы, откуда ты хочешь получать новости, а затем нажми "Ок"',
                          reply_markup=markup)
@@ -131,9 +113,8 @@ def news_source(message):
 
     if message.text == 'Сбросить группы':
         group_id_arr = []
-        # db.execute("UPDATE Users SET group_1 = 0, group_2 = 0, group_3 = 0, group_4 = 0, group_5 = 0, group_6 = 0 "
-        #            " WHERE id = ?", (message.chat.id,))
-        # database.commit()
+        db.execute("DELETE FROM UsersGroups WHERE uid = ?", (message.chat.id,))
+        database.commit()
         bot.send_message(message.chat.id, 'Группы были сброшены', reply_markup=markup_settings)
     if message.text == 'Остановить подписку':
         global sub_is_active
@@ -151,7 +132,7 @@ def news_source(message):
         bot.send_message(message.chat.id, 'Что ты хочешь получить?', reply_markup=markup1)
 
     if message.text == '5 последних постов':
-        vk_arr = vkfunction(group_id_arr)
+        vk_arr = five_last_posts(message)
         if vk_arr:
             for _i in vk_arr:
                 bot.send_message(message.chat.id, _i)
@@ -189,32 +170,23 @@ def group_selection(msg, grp_id):
     dtbs.close()
 
 
-def vkfunction(vk_id_arr):
+def five_last_posts(msg):
     arr_link = []
-    vk_json_arr = []
     link_count = 0
+    dtbs = sqlite3.connect('HSE_BOT_DB.sqlite')
+    dtbs_c = dtbs.cursor()
 
-    for _j in vk_id_arr:
-        group = vk_api.wall.get(owner_id='-' + _j, count=6, filter='owner')
-        print(group)
-        post_count = 0
-        for _k in group['items']:
-            if type(_k) != int:
-                if 'id' in _k:
-                    if 'is_pinned' not in _k and post_count < 5:
-                        vk_json_arr.append(_k)
-                        post_count += 1
+    dtbs_c.execute("SELECT p.id FROM Posts as p, UsersGroups as ug WHERE ug.uid = ? AND ug.gid = p .gid "
+                   "ORDER BY p.date DESC ", (msg.chat.id,))
+    flp = dtbs_c.fetchall()
+    for i in flp:
+        link = 'https://vk.com/wall-' + str(i[0])
+        if link_count < 5:
+            arr_link.append(link)
+            link_count += 1
+            print(link)
 
-    vk_sorted_arr = sorted(vk_json_arr, key=lambda k: int(k['date']), reverse=True)
-
-    for _n in vk_sorted_arr:
-        for _j in vk_id_arr:
-            if '-' + _j == str(_n['owner_id']):
-                link = 'https://vk.com/wall-' + _j + '_' + str(_n['id'])
-                if link_count < 5:
-                    arr_link.append(link)
-                    link_count += 1
-                    print(link)
+    dtbs.close()
     return arr_link
 
 
@@ -253,6 +225,34 @@ def vk_sub(id_vk, last_post_date):
         _pd += 1
     print(main_list)
     return main_list
+
+
+def print_vk_sub(msg, last_post_date):
+    last_posts = vk_sub(group_id_arr, last_post_date)
+    last_dates = []
+    ret_dates = []
+    for j in last_posts:
+        if j:
+            last_dates.append(j[0])
+            print(last_posts)
+            for i in range(1, len(j), 2):
+                bot.send_message(msg.chat.id, j[i])
+        elif not j:
+            last_dates.append(None)
+
+    for k in range(0, len(last_post_date)):
+        if last_dates[k] is not None:
+            print(last_dates[k])
+            ret_dates.append(last_dates[k])
+        else:
+            print(last_post_date[k])
+            ret_dates.append(last_post_date[k])
+
+    print(ret_dates)
+    t = threading.Timer(3, print_vk_sub, [msg, ret_dates])
+    t.start()
+    if not sub_is_active:
+        t.cancel()
 
 
 if __name__ == '__main__':
